c0c66aedfc2c867db63f39d446a2aee18333b2d4
Nikhil Sharma <nikhilsharma@MacBook-Pro-5.local>
2022-10-02 18:55:14 +0530

    Created child component to display Robo information

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Card extends React.Component{
+    render(){
+        return(
+            <div className='tc ba b--solid br2 bg-light-yellow bw1 fl w-third avenir measure grow grow:hover pa2 ma2'>
+            <img alt='roboPhoto' src={`https://robohash.org/${this.props.id}?200x200`}/>
+                <div>
+                <h1>{this.props.name}</h1>
+                <p>{this.props.email}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Card;
