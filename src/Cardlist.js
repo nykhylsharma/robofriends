a276aaa673730c69175ca97029e07680c16f3422
Nikhil Sharma <nikhilsharma@MacBook-Pro-5.local>
2022-10-02 21:09:26 +0530

    Propagate CardList with cards as per robots.js file

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -7,7 +7,17 @@ class Cardlist extends React.Component{
     render(){
         return(
         <div>
-            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+        {
+            robots.map((robot,index)=>{
+                return(
+                    <Card 
+                    key={index} 
+                    id={robots[index].id} 
+                    name={robots[index].name} 
+                    email={robots[index].email}/>
+                )
+            })
+        }   
         </div> 
         );      
     }
